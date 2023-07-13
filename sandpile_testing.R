085cf2b72fec9a347d0300462ccee28862edf514
gulbord <52690645+gulbord@users.noreply.github.com>
2023-07-13 18:27:45 +0200

    Exploratory analysis on sandpile.R

--- sandpile_testing.R
+++ sandpile_testing.R
@@ -1,4 +1,9 @@
+library(data.table)
 library(igraph)
+library(ggplot2)
+theme_set(
+  theme_minimal(base_size = 15, base_family = "Fira Sans Condensed")
+)
 source("sandpile.R")
 
 g <- sample_fitness_pl(
@@ -12,3 +17,21 @@ plot(
   vertex.color = "firebrick", edge.color = "grey80",
   vertex.label = NA, vertex.size = sqrt(degree(g))
 )
+
+sp <- sandpile(g, 1e5, 10^-4)
+
+# save in datatable all variables except loads
+sp_dt <- as.data.table(sp[-1])
+
+n_avs <- nrow(sp_dt)
+
+# subset events before the first whole-network avalanche
+# ("before full capacity")
+sp_bfc <- sp_dt[1:(min(which(areas == 1000)) - 1)]
+time <- seq_len(nrow(sp_bfc))
+
+sp_bfc[areas != sizes]
+
+ggplot(sp_bfc, aes(x = seq_len(nrow(sp_bfc)))) +
+  geom_line(aes(y = areas), colour = "black") +
+  geom_line(aes(y = sizes), colour = "firebrick")
