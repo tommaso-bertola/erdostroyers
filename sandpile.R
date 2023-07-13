d1fc9feacf7c96ae30cde73caf6fd382315171e0
alessiopitteri <alessio.pitteri@studenti.unipd.it>
2023-07-13 17:22:15 +0200

    clusters error

--- sandpile.R
+++ sandpile.R
@@ -1,12 +1,19 @@
 library(igraph)
 
-sandpile <- function(graph, n_iters) {
+sandpile <- function(graph, n_iters, sink_frac) {
+  
+  if(clusters(graph)$no > 1) {
+    return(print('graph not connected'))
+  }
+  
   degrees <- degree(graph)
   adj_mat <- get.adjacency(graph)
   # number of nodes
   n <- length(degrees)
   # "grains" in each node
   loads <- rep(0, length(degrees))
+  
+  durations <- c()
 
   for (i in 1:n_iters) {
     # choose node at random
@@ -17,18 +24,35 @@ sandpile <- function(graph, n_iters) {
     if (loads[pick] >= degrees[pick]) {
       loads[pick] <- 0
       # select neighbours and offload
-      nbs <- which(adj_mat[, pick] == 1)
+      nbs <- adj_mat[, pick, drop=F]@i + 1
       loads[nbs] <- loads[nbs] + 1
 
       overs <- which(loads >= degrees)
+      count <- 1
+      
       while (length(overs) > 0) {
         loads[overs] <- 0
-        nbs <- which(adj_mat[, overs] == 1)
+        nbs <- adj_mat[, overs, drop=F]@i + 1
+        nbs <- nbs[runif(length(nbs)) > sink_frac]
         loads[nbs] <- loads[nbs] + 1
 
         # update overflown nodes
         overs <- which(loads >= degrees)
+        count <- count + 1
       }
+      durations <- c(durations, count) 
     }
   }
+  return(
+    list(
+      loads = loads,
+      durations = durations
+    )
+  )
 }
+
+g <- sample_fitness_pl(
+  no.of.nodes = 1000, no.of.edges = 5000, exponent.out = 2.5
+)
+
+sandpile(g, 200, 10**-4)
