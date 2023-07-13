068033f76ff550aac01a2c8b016e519b0bcbd641
gulbord <52690645+gulbord@users.noreply.github.com>
2023-07-13 18:27:26 +0200

    Save all avalanche parameters

--- sandpile.R
+++ sandpile.R
@@ -12,8 +12,11 @@ sandpile <- function(graph, n_iters, sink_frac) {
 
   durations <- c()
 
-  # avalanche length (timesteps)
+  # avalanche parameters
   durations <- c()
+  areas <- c()
+  sizes <- c()
+  grains <- c()
 
   for (i in 1:n_iters) {
     # choose node at random
@@ -22,30 +25,52 @@ sandpile <- function(graph, n_iters, sink_frac) {
 
     # check if the node is overflowing
     if (loads[pick] >= degrees[pick]) {
+      d_cnt <- 1
+      # for the area, track the unique indices
+      # of affected notes
+      a_cnt <- pick
+      s_cnt <- 1
+      g_cnt <- loads[pick]
+
       loads[pick] <- 0
-      count <- 1
       # select neighbours and offload
       nbs <- adj_mat[, pick, drop = FALSE]@i + 1
       loads[nbs] <- loads[nbs] + 1
 
+      # check if avalanche is proceeding
       overs <- which(loads >= degrees)
-      count <- 1
       while (length(overs) > 0) {
+        # update counters
+        d_cnt <- d_cnt + 1
+        a_cnt <- union(a_cnt, overs)
+        s_cnt <- s_cnt + length(overs)
+        g_cnt <- g_cnt + sum(loads[overs])
+
         loads[overs] <- 0
         nbs <- adj_mat[, overs, drop = FALSE]@i + 1
         # drop grains with probability sink_frac
         nbs <- nbs[runif(length(nbs)) > sink_frac]
-        loads[nbs] <- loads[nbs] + 1
+        for (n in nbs) {
+          loads[n] <- loads[n] + 1
+        }
 
         # update overflown nodes
         overs <- which(loads >= degrees)
-        count <- count + 1
       }
 
-      # save avalanche duration
-      durations <- c(durations, count)
+      # save avalanche parameters
+      durations <- c(durations, d_cnt)
+      areas <- c(areas, length(a_cnt))
+      sizes <- c(sizes, s_cnt)
+      grains <- c(grains, g_cnt)
     }
   }
 
-  list(loads = loads, durations = durations)
+  list(
+    loads = loads,
+    durations = durations,
+    areas = areas,
+    sizes = sizes,
+    grains = grains
+  )
 }
