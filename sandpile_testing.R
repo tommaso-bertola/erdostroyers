f04036e1762df9a98a0672a14a51ece1750d2933
gulbord <52690645+gulbord@users.noreply.github.com>
2023-07-15 17:51:49 +0200

    Some more testing

--- sandpile_testing.R
+++ sandpile_testing.R
@@ -6,20 +6,22 @@ theme_set(
 )
 source("sandpile.R")
 
-g <- sample_fitness_pl(
-  no.of.nodes = 1000,
-  no.of.edges = 5000,
-  exponent.out = 2.5
-)
-clusters(g)$no # test number of clusters
+repeat {
+  g <- sample_fitness_pl(
+    no.of.nodes = 1000,
+    no.of.edges = 4000,
+    exponent.out = 3
+  )
+  if (clusters(g)$no == 1) break
+}
 
 plot(
-  g, layout = layout_with_fr(g),
+  g, layout = layout_with_kk(g),
   vertex.color = "firebrick", edge.color = "grey80",
   vertex.label = NA, vertex.size = sqrt(degree(g))
 )
 
-sp <- sandpile(g, n_iters = 5e3, sink_frac = 10^-4, sample_freq = 50)
+sp <- sandpile(g, n_iters = 7e3, sink_frac = 10^-4, sample_freq = 50)
 
 g_data <- data.table(
   node = 1:vcount(g),
@@ -41,3 +43,14 @@ ggplot(data = loads, aes(x = fill_perc)) +
     expand = expansion(mult = c(0, 0.1))
   ) +
   labs(x = "Filled capacity (%)", y = "Count")
+
+g_data <- data.table(node = 1:vcount(g), degree = degree(g))
+g_data[, sons := purrr::map_int(node, ~ degree[.x] * sum(sp$toppled == .x))]
+
+ggplot(g_data[sons > 0]) +
+  geom_histogram(aes(x = sons), fill = "firebrick", binwidth = 1)
+
+ggplot(g_data) +
+  geom_histogram(aes(x = degree), fill = "firebrick", binwidth = 1)
+
+g <- make_graph(~ 1--2, 2--3, 2--4, 2--5, 4--5)
