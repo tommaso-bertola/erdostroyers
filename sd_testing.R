7aba8323f5285331254211f6f33a4f955f423cb0
gulbord <52690645+gulbord@users.noreply.github.com>
2023-07-16 18:56:11 +0200

    Generate table with counts

--- sd_testing.R
+++ sd_testing.R
@@ -5,15 +5,15 @@ theme_set(
 )
 source("size_distribution_sf.R")
 
-gammas <- c(2, 2.25, 2.5, 2.75)
+gammas <- c(2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9)
 n <- 1000
 m <- 5000
 n_iters <- 5000
 sink_frac <- 1e-4
 sizes <- size_dist_sf(gammas, n_samples = 100, n, m, n_iters, sink_frac)
 
-s_counts <- sizes[, .(s = unlist(unique(sizes))), keyby = gamma
-      ][, .(count = .N), keyby = .(gamma, s)]
+s_counts <- sizes[, .(s = unlist(unique(sizes))), by = gamma
+                  ][, .(count = .N), keyby = .(gamma, s)]
 
 ggplot(s_counts, aes(x = s, y = count)) +
   geom_point(aes(colour = factor(gamma), shape = factor(gamma))) +
