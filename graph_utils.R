7fe55346d70cfe7b0c401b3ea271d57036f7d717
gulbord <52690645+gulbord@users.noreply.github.com>
2023-07-17 17:02:44 +0200

    Fix typo

--- graph_utils.R
+++ graph_utils.R
@@ -2,7 +2,7 @@ get_lcc <- function(graph) {
   require(igraph)
 
   components <- clusters(graph)
-  lcc_id <- which.max(components$size)
+  lcc_id <- which.max(components$csize)
   lcc_verts <- V(graph)[components$membership == lcc_id]
   return(induced_subgraph(graph, lcc_verts))
 }
