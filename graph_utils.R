fbe25ad1a0ba724f8d9d21bce94a76df9bf6788d
gulbord <52690645+gulbord@users.noreply.github.com>
2023-07-17 17:00:41 +0200

    Add graph utilities like get_lcc()

---
+++ graph_utils.R
@@ -0,0 +1,8 @@
+get_lcc <- function(graph) {
+  require(igraph)
+
+  components <- clusters(graph)
+  lcc_id <- which.max(components$size)
+  lcc_verts <- V(graph)[components$membership == lcc_id]
+  return(induced_subgraph(graph, lcc_verts))
+}
