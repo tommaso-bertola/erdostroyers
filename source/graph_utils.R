d1d7b67790c52663d52d53c1b8ebec74e2db25f1
alessiopitteri <alessio.pitteri@studenti.unipd.it>
2023-07-18 16:15:51 +0200

    top_matrix

--- source/graph_utils.R
+++ source/graph_utils.R
@@ -7,3 +7,20 @@ get_lcc <- function(graph, mode = "weak") {
   lcc_verts <- V(graph)[components$membership == lcc_id]
   return(induced_subgraph(graph, lcc_verts))
 }
+
+get_top_matrix <- function(graph, sp){
+  n <- vcount(g)
+  top_matrix <- matrix(0, nrow = n, ncol = n)
+  for(top in sp$toppled){
+    top <- sp$toppled[[509]]
+    for(i in seq_along(top)){
+      for(j in (i+1):length(top)) {
+        if((i+1)<=length(top)){ 
+          top_matrix[top[i], top[j]] <- top_matrix[top[i], top[j]] + 1
+        }
+      }
+    }
+  }
+  
+  return(top_matrix)
+}
