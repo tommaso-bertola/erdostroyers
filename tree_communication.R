e5e5240a1a31010bb85c2c12623905076baefece
maxmatzler <matzler.j.maximilian@gmail.com>
2023-07-21 17:08:00 +0200

    changes tree communication

--- tree_communication.R
+++ tree_communication.R
@@ -2,7 +2,7 @@ library(igraph)
 if(T) {
   load("tree.RData")
   num_nodes = length(V(g)$name)
-  print("using BA-like network")
+  print("using hierarchical tree network")
 } else {
   nodes_df = read.csv("network/nodes_cleaned.csv")
   edges_df = read.csv("network/edges.csv")
@@ -16,7 +16,7 @@ if(T) {
 d = distances(g)
 
 #new_packets = c(0.5, 1.5, 3, 10)
-new_packets=1
+new_packets= c(1,2,3)
 
 tmax = 1000
 
@@ -60,39 +60,37 @@ for(pi in 1:length(new_packets)) {
     to_remove = c()
     if(nrow(packets)!=0) {
       for(j in 1:nrow(packets)) {
-        if(nodes_queue[[packets[j,"curr"]]][1] == packets[j, "id"]) {
-          nodes_queue[[packets[j,"curr"]]] = nodes_queue[[packets[j,"curr"]]][-1]
           #decide movement
           a=d[packets[j, "curr"],]
           neighbors = (1:num_nodes)[a == 1]
           distances = h * d[neighbors, packets[j, "dest"]] + (1-h) * sapply(nodes_queue[neighbors], length)
           closest_neighbors = neighbors[distances == min(distances)]
-          #following line is the only relevant line for the different assigning strategy
+          #following line different assigning strategy
           #packets[j, "curr"] = closest_neighbors[sample(length(closest_neighbors), 1)]
-          ##################start of differing part according to closest neighbors
           #first sample one of the closest neighbors randomly, then compute q_ij
           closest_neighbor = closest_neighbors[sample(length(closest_neighbors), 1)]
-          gamma = 2
-          xab = runif(1, min=0, max=1) #change to one to switch off noise
-          fa = ifelse(length(nodes_queue[[packets[j,"curr"]]])>0, length(nodes_queue[[packets[j,"curr"]]])>0, 1)
+          gamma = 1.5
+          #xab = runif(1, min=0, max=1) #change to one to switch off noise
+          xab = 1
+          fa = ifelse(length(nodes_queue[[packets[j,"curr"]]])>0, length(nodes_queue[[packets[j,"curr"]]]), 1)
           kab = xab*fa^(-gamma)
-          xba = runif(1, min=0, max=1) #change to one to switch off noise
-          fb = ifelse(length(nodes_queue[[closest_neighbor]])>0, length(nodes_queue[[packets[j,"curr"]]])>0, 1)
+          #xba = runif(1, min=0, max=1) #change to one to switch off noise
+          xba = 1
+          fb = ifelse(length(nodes_queue[[closest_neighbor]])>0, length(nodes_queue[[closest_neighbor]]), 1)
           kba = xba*fb^(-gamma)
-          qab = sqrt(xab*kab*xba*kba)
+          qab = sqrt(kab*kba)
           hop = runif(1, 0, 1)
           if(hop<qab){
-            packets[j, "curr"] = closest_neighbors[sample(length(closest_neighbors), 1)]
-            nodes_queue[[packets[j, "curr"]]] = c(nodes_queue[[packets[j, "curr"]]], packets[j, "id"])
+            nodes_queue[[packets[j,"curr"]]] = nodes_queue[[packets[j,"curr"]]][-1]
+            packets[j, "curr"] = closest_neighbor
+            #i put this block inside here
+            if(packets[j, "curr"] == packets[j, "dest"]) {
+              to_remove = c(to_remove, j)
+              times = c(times, packets[j, "time"])
+            } else {
+              nodes_queue[[packets[j, "curr"]]] = c(nodes_queue[[packets[j, "curr"]]], packets[j, "id"])
+            }
           }
-          ##################end of differing part according to closest neighbors
-          if(packets[j, "curr"] == packets[j, "dest"]) {
-            to_remove = c(to_remove, j)
-            times = c(times, packets[j, "time"])
-          } else {
-            nodes_queue[[packets[j, "curr"]]] = c(nodes_queue[[packets[j, "curr"]]], packets[j, "id"])
-          }
-        }
         packets[j, "time"] = packets[j, "time"] + 1
       }
     }
@@ -105,3 +103,15 @@ for(pi in 1:length(new_packets)) {
   print(length(times)/(p_id)) #fraction of packets arriving
   print(mean(packets[,"time"])) #mean time of packets not arriving
 }
+
+png("A_net")
+plot(A[length(new_packets),], type="l", main=NA, xlab="time", ylab="Active packets")
+if(length(new_packets)!=1) {
+  for(i in 1:(length(new_packets)-1)) {
+    lines(A[i,])
+  }
+}
+dev.off()
+
+###order parameter
+
