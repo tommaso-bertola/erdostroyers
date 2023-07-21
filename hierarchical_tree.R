bdcbf46126f1dd60add7ae4d781fbe80ac65832b
maxmatzler <matzler.j.maximilian@gmail.com>
2023-07-21 16:50:23 +0200

    added properties of graph

--- hierarchical_tree.R
+++ hierarchical_tree.R
@@ -1,14 +1,22 @@
 library(igraph)
 
 m = 3 #number of levels
-z = 3 #branching factor
+z = 5 #branching factor
 n = 0 #number of nodes
 for(i in 0:m){
   n = n + z^i
 }
 
+pc = sqrt(z)/(z*(z^(m-1)-1)^2/(z^m - 1) + 1) #critical probability before congestion
+
 g = make_tree(n, children = z, mode = "undirected")
 V(g)$name = 1:n
 plot(g, vertex.label=NA, vertex.size=0.2)
 
+d = distances(g)
+d_avg = average.path.length(g)
+
 save(g, file="tree.RData")
+
+#degree distribution obvious
+#clustering coefficient basically zero
