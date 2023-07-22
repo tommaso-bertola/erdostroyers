80106d67e48b70556088f4de5688cfedb52a7b5b
Davide <bacilieri.davide@gmail.com>
2023-07-22 13:34:17 +0200

    added na count dependent coloring

--- rho_graph.R
+++ rho_graph.R
@@ -5,11 +5,15 @@ df = df[-1,]
 
 y=c()
 s=c()
+n=c()
 
 for(i in 1:length(x)) {
  y = c(y, mean(df[,i], na.rm=T))
  s = c(s, sd(df[,i], na.rm=T))
+ n = c(n, sum(is.na(df[,i]))+1)
 }
 
-plot(x, y, ylim=c(-0.1, 1.1))
-arrows(x, y+s, x, y-s, code=3, len=0.05, angle=90)
+colors = c("black", "gray50", "red", "pink", "orange")
+
+plot(x, y, ylim=c(-0.1, 1.1), col=colors[n])
+arrows(x, y+s, x, y-s, code=3, len=0.05, angle=90, col=colors[n])
