33b4543c9fdc69f9d8cc5ebef4c0a57f9b2d4170
gulbord <52690645+gulbord@users.noreply.github.com>
2023-07-22 07:25:52 +0200

    Clean up and add log binning

--- sd_testing.R
+++ sd_testing.R
@@ -2,70 +2,59 @@ library(igraph)
 library(data.table)
 library(ggplot2)
 theme_set(
-  theme_bw(base_size = 10, base_family = "Fira Sans Condensed")
+  theme_bw(base_size = 15, base_family = "Fira Sans Condensed")
 )
 source("source/sandpile.R")
 source("source/size_distribution_sf.R")
-
-repeat {
-  g <- sample_fitness_pl(
-    no.of.nodes = 1e3,
-    no.of.edges = 1e4,
-    exponent.out = 2.5
-  )
-  if (clusters(g)$no == 1) break
-}
-
-sp <- sandpile(g, 2.5e4, 0.01, stop_at_soc = T)
-sizes <- data.table(
-  s = sp$sizes[5000:length(sp$sizes)]
-)[, .(.N), keyby = s]
-
-plot(log(sizes$s), log(sizes$N))
-
-gammas <- c(2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9)
-n <- 1e3
-m <- 5e3
-n_iters <- 1e4
-sink_frac <- 1e-4
-sizes <- size_dist_sf(gammas, n_samples = 100, n, m, n_iters, sink_frac)
-
-s_counts <- sizes[, .(s = unlist(unique(sizes))), by = gamma
-                  ][, .(count = .N), keyby = .(gamma, s)]
-
-s_counts[gamma %in% c(2.3, 2.5, 2.7, 2.9)] |>
-  ggplot(aes(x = s, y = count)) +
-    geom_point(
-      aes(colour = factor(gamma), shape = factor(gamma)),
-      size = 1.25
-    ) +
-    geom_line(aes(colour = factor(gamma)), linewidth = 0.5) +
-    scale_x_continuous(trans = "log10") +
-    scale_y_continuous(breaks = c(1, 1e2, 1e4, 1e6), trans = "log10") +
-    scale_colour_viridis_d(option = "viridis") +
-    annotation_logticks() +
-    labs(
-      x = "Avalanche size S", y = "Count",
-      shape = "Exponent γ", colour = "Exponent γ"
-    )
-
-ggsave(
-  "size_dist_sf.png", device = "png",
-  width = 13, height = 10, units = "cm",
-)
-
-source("graph_utils.R")
+source("source/graph_utils.R")
 
 g <- sample_fitness_pl(
-  no.of.nodes = 4941,
-  no.of.edges = 6594,
-  exponent.out = 2.5
-) |> get_lcc()
-
-d_counts <- data.table(deg = degree(g))[, .(n = .N), by = deg
-                                        ][, lapply(.SD, log)]
-
-sp <- sandpile(
-  g, n_iters = length(V(g)) + length(E(g)),
-  sink_frac = 1e-4, sample_freq = 100
+  no.of.nodes = 1e4,
+  no.of.edges = 2e4,
+  exponent.out = 2.1
 )
+g <- get_lcc(g)
+
+sp <- sandpile(g, 8e4, 0.01)
+
+gammas <- c(2.1, 2.3, 2.5, 2.7, 2.9)
+n <- 1e4
+m <- 2e4
+n_samples <- 50
+n_iters <- 8e4
+sink_frac <- 0.01
+start_ind <- 2e4
+size_dist_sf(gammas, n_samples, n, m, n_iters, sink_frac, start_ind) |>
+  fwrite("size_dist.out")
+
+logbins <- function(data, base = 10, dlog = 0.1) {
+  # determine counts from data
+  cnt <- data.frame(val = unique(data))
+  cnt$n <- sapply(cnt$val, \(x) sum(data == x))
+  cnt <- cnt[order(cnt$val), ]
+
+  # get (log-)midpoints and lower/upper limits (to nearest integer)
+  log_mid <- seq(0, log(max(cnt$val), base), by = dlog)
+  lower <- ceiling(base^(log_mid - 0.5 * dlog))
+  upper <- floor(base^(log_mid + 0.5 * dlog))
+
+  # select only bins larger than two, e.g. lower = 9, upper = 11
+  bin_window <- which(upper > lower + 1)
+  lower <- lower[bin_window]
+  upper <- upper[bin_window]
+
+  # the values before bin_window are taken directly from the counts
+  x <- seq(1, lower[1] - 1)
+  y <- c(
+    cnt$n[cnt$val %in% x], # these are from the counts
+    # here we average the counts falling inside each remaining bin
+    mapply(
+      \(l, u) sum(cnt$n[l <= cnt$val & cnt$val <= u]) / (1 + u - l),
+      lower, upper
+    )
+  )
+  # take the remaining x values from the midpoints defined before
+  x <- c(x, base^log_mid[bin_window])
+
+  return(data.frame(x = x, y = y))
+}
